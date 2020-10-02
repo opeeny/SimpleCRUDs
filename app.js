eeb93f1d7c5629099f020ee7cdd1931a8539cd31
opeeny <godfreyopeeny@gmail.com>
2020-10-02 10:51:23 +0300

    Connect to Mangodb database test

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
-const port = "3000"
-console.log("Hellow, May Node be with you");
+const bodyPaser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient
+const port = "3000";
 
-//create a server that browsers can listen to
+const uri = "mongodb+srv://admin:admin@cluster0.clsfs.mongodb.net/test?retryWrites=true&w=majority";
+
+//DB
+MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(client => {
+    const collection = client.db("test").collection("devices");
+
+    console.log("Connected to the Database");
+    client.close;
+    
+})
+.catch(error => console.log(err))
+        
+
+//use Body-parse
+app.use(bodyPaser.urlencoded({ extended: true }));
+
+//get handler, create a server that browsers can listen to
 app.get("/", (req, res) => {
    //res.send(__dirname + '/index.html');
    res.sendFile(__dirname + '/index.html');
 //    console.log("Its a quick check");
 });
+//Handle a post request
+app.post("/quotes", (req, res) => {
+    console.log(req.body);
+});
 
+//PORT
 app.listen(port, () => {
     console.log(__dirname);
     console.log(`The browser is now listening to the server at ${port}`);
